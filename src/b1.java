1429f9b5451d6ac7da39b326aac15bfdcff5b6e5
hien910 <hienjang910@gmail.com>
2023-08-29 10:49:41 +0700

    .

---
+++ src/b1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class b1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ax + b = 0");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+//        System.out.println((double) -b/a);
+        System.out.println("Nghiệm của phương  trình là "+ (double) -b/a);
+
+    }
+}
